f150fc00d88f8aa2809b70223ab0e11803ba5f61
10994015 <a0938599191@gmail.com>
2025-08-13 00:26:22 +0800

    請求超時10分鐘

--- src/axios.js
+++ src/axios.js
@@ -6,7 +6,7 @@ const axiosClient = axios.create({
   baseURL: '/api',  // 使用代理路徑
   // baseURL: import.meta.env.VITE_API_BASE_URL + 'api',  // 正式區路徑
   withCredentials: true,  // 允許發送 cookies
-  timeout: 100000,  // 請求超時時間 (10秒)
+  timeout: 600000,  // 請求超時時間 (10秒)
   headers: {
     'Content-Type': 'application/json'
   }
